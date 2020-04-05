6ea2ca5ed0f7898fd8bee9355a337abe462709c8
Paden Tomasello <padentomasello@devfair049.maas>
2020-04-04 19:14:44 -0700

    Actually add module to Container

--- flashlight/contrib/modules/Resnet.cpp
+++ flashlight/contrib/modules/Resnet.cpp
@@ -79,15 +79,15 @@ ConvBnAct::ConvBnAct(
 ResNetBlock::ResNetBlock() = default;
 
 ResNetBlock::ResNetBlock(const int in_c, const int out_c, const int stride) {
-  if (in_c != out_c || stride == 2) {
-    downsample_ = std::make_shared<ConvBnAct>(in_c, out_c, 1, 1, stride, stride, true, false);
-  }
   add(std::make_shared<Conv2D>(conv3x3(in_c, out_c, stride, 1)));
   add(std::make_shared<BatchNorm>(batchNorm(out_c)));
   add(std::make_shared<ReLU>());
   add(std::make_shared<Conv2D>(conv3x3(out_c, out_c, 1, 1)));
   add(std::make_shared<BatchNorm>(batchNorm(out_c)));
   add(std::make_shared<ReLU>());
+  if (in_c != out_c || stride == 2) {
+    add(ConvBnAct(in_c, out_c, 1, 1, stride, stride, true, false));
+  }
   //add(std::make_shared<ConvBnAct>(in_c, out_c, 3, 3, stride, stride));
   //add(std::make_shared<ConvBnAct>(out_c, out_c, 3, 3, 1, 1, true, false));
   //add(std::make_shared<ReLU>());
@@ -109,8 +109,8 @@ std::vector<fl::Variable> ResNetBlock::forward(
   out = bn2->forward(out);
 
   std::vector<fl::Variable> shortcut;
-  if (downsample_) {
-    shortcut = downsample_->forward(inputs);
+  if (modules().size() > 6) {
+    shortcut = module(6)->forward(inputs);
   } else {
     shortcut = inputs;
   }
