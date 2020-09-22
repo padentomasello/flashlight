68d66d9e8f6232fdae2b0a2caf23f43c14b3624d
Paden Tomasello <padentomasello@devfair049.maas>
2020-09-21 20:22:44 -0700

    Data dir pass through

--- wrapper_detr.sh
+++ wrapper_detr.sh
@@ -16,6 +16,7 @@ echo $SLURM_LOCALID
 # Needed for arrayfire
 export LD_LIBRARY_PATH=/private/home/padentomasello/usr/lib/:$LD_LIBRARY_PATH
 
+DATA_DIR=${DATA_DIR:-/private/home/padentomasello/data/coco3/}
 BUILD_DIR=/scratch/slurm_tmpdir/$SLURM_JOB_ID/$1
 EVAL_DIR=$BUILD_DIR/eval/$SLURM_LOCALID/
 mkdir -p $BUILD_DIR/rndv/
@@ -24,7 +25,8 @@ $BUILD_DIR/flashlight/build/Detr -lr 0.0001 --epochs 100000 --batch_size 16 \
 --world_rank $SLURM_LOCALID --world_size $SLURM_NTASKS \
 --rndv_filepath $BUILD_DIR/rndv/ \
 --checkpointpath /checkpoint/padentomasello/models/detr \
---eval_dir $EVAL_DIR
+--eval_dir $EVAL_DIR \
+--data_dir $DATA_DIR
 
 
 
