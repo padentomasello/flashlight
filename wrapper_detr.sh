af83b38ae791af5c38e10ddabd47896fef204e61
Paden Tomasello <padentomasello@devfair049.maas>
2020-10-23 13:55:11 -0700

    Try 480

--- wrapper_detr.sh
+++ wrapper_detr.sh
@@ -26,6 +26,7 @@ $BUILD_DIR/flashlight/build/Detr -lr 0.0001 --epochs 100000 --batch_size 8 \
 --checkpointpath /checkpoint/padentomasello/models/detr \
 --eval_dir $EVAL_DIR \
 --tryfromenv=eval_iters,data_dir,metric_iters,pretrained,print_params
+--onesize
 
 
 
