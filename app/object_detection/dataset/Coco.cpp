2debe8ca18b828b0260a4ca216639953839fb5d4
Paden Tomasello <padentomasello@devfair049.maas>
2020-10-26 10:30:04 -0700

    No one size

--- app/object_detection/dataset/Coco.cpp
+++ app/object_detection/dataset/Coco.cpp
@@ -428,7 +428,8 @@ CocoDataset::CocoDataset(
          transformed, randomResize({256}, 800));
   } else {
     transformed = std::make_shared<TransformAllDataset>(
-         transformed, randomResize({480, 512}, 800));
+         transformed, randomResize({480, 512, 544, 576, 608, 640, 672, 704, 736, 768, 800}, 800));
+         //transformed, randomResize({480, 512}, 800));
          //transformed, randomResize({256}, 800));
    }
 
