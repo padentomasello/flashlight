6f00c0d6e7e11a966d6344612412bd68978eef50
Paden Tomasello <padentomasello@devfair049.maas>
2020-12-10 15:58:25 -0800

    Add dataset

--- flashlight/app/objdet/examples/Detr.cpp
+++ flashlight/app/objdet/examples/Detr.cpp
@@ -14,6 +14,7 @@
 #include "flashlight/app/objdet/dataset/Coco.h"
 #include "flashlight/app/objdet/nn/PositionalEmbeddingSine.h"
 #include "flashlight/app/objdet/nn/Transformer.h"
+#include "flashlight/app/objdet/nn/Detr.h"
 
 #include "flashlight/ext/common/DistributedUtils.h"
 #include "flashlight/ext/image/af/Transforms.h"
@@ -57,98 +58,6 @@ using namespace fl;
 using namespace fl::ext::image;
 using namespace fl::app::objdet;
 
-// TODO Refactor
-//const int32_t backboneChannels = 512;
-
-class MLP : public Sequential {
-
-public: MLP(const int32_t inputDim,
-      const int32_t hiddenDim,
-      const int32_t outputDim,
-      const int32_t numLayers)
-  {
-    add(Linear(inputDim, hiddenDim));
-    for(int i = 1; i < numLayers - 1; i++) {
-      add(ReLU());
-      add(Linear(hiddenDim, hiddenDim));
-    }
-    add(ReLU());
-    add(Linear(hiddenDim, outputDim));
-  }
-};
-
-class Detr : public Container {
-
-public:
-
-  Detr(
-      //std::shared_ptr<Module> backbone,
-      std::shared_ptr<Transformer> transformer,
-      const int32_t hiddenDim,
-      const int32_t numClasses,
-      const int32_t numQueries,
-      const bool auxLoss) :
-    //backbone_(backbone),
-    transformer_(transformer),
-    numClasses_(numClasses),
-    numQueries_(numQueries),
-    auxLoss_(auxLoss),
-    classEmbed_(std::make_shared<Linear>(hiddenDim, numClasses + 1)),
-    bboxEmbed_(std::make_shared<MLP>(hiddenDim, hiddenDim, 4, 3)),
-    queryEmbed_(std::make_shared<Embedding>(hiddenDim, numQueries)),
-    inputProj_(std::make_shared<Conv2D>(512, hiddenDim, 1, 1)),
-    posEmbed_(std::make_shared<PositionalEmbeddingSine>(hiddenDim / 2,
-          10000, true, 6.283185307179586f))
-  {
-    //add(backbone_);
-    add(bboxEmbed_);
-    add(classEmbed_);
-    add(inputProj_);
-    add(posEmbed_);
-    add(queryEmbed_);
-    add(transformer_);
-  }
-
-  std::vector<Variable> forward(const std::vector<Variable>& input) {
-
-    //auto backboneFeatures = backbone_->forward(input);
-    auto features = input[0];
-    auto mask = input[1];
-    auto backboneFeatures = input;
-    auto inputProjection = inputProj_->forward(features);
-    auto posEmbed = posEmbed_->forward({mask})[0];
-    //return { inputProjection, posEmbed };
-    auto hs = transformer_->forward(
-        inputProjection,
-        mask,
-        queryEmbed_->param(0),
-        posEmbed);
-
-    auto outputClasses = classEmbed_->forward(hs[0]);
-    auto outputCoord = sigmoid(bboxEmbed_->forward(hs)[0]);
-
-    return { outputClasses, outputCoord };
-  }
-
-  std::string prettyString() const {
-    // TODO print params
-    return "Detection Transformer!";
-  }
-
-private:
-  std::shared_ptr<Module> backbone_;
-  std::shared_ptr<Transformer> transformer_;
-  std::shared_ptr<Linear> classEmbed_;
-  std::shared_ptr<MLP> bboxEmbed_;
-  std::shared_ptr<Embedding> queryEmbed_;
-  std::shared_ptr<PositionalEmbeddingSine> posEmbed_;
-  std::shared_ptr<Conv2D> inputProj_;
-  int32_t hiddenDim_;
-  int32_t numClasses_;
-  int32_t numQueries_;
-  bool auxLoss_;
-
-};
 
 void printParamsAndGrads(std::shared_ptr<fl::Module> mod) {
   auto params = mod->params();
