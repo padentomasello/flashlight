3ed753e2363c5353e95c5e03ee396a4d3688a743
Paden Tomasello <padentomasello@devfair049.maas>
2021-02-04 13:13:34 -0800

    Test

--- flashlight/app/objdet/criterion/SetCriterion.cpp
+++ flashlight/app/objdet/criterion/SetCriterion.cpp
@@ -276,12 +276,14 @@ SetCriterion::LossDict SetCriterion::forward(
           [](int curr, const Variable& label) { return curr + label.dims(1);  }
       );
 
+      std::cout << "Rank: " << fl::getWorldRank() << " Num boxes pre" << numBoxes << std::endl;
       af::array numBoxesArray = af::constant(numBoxes, { 1 }, af::dtype::s32);
       if (isDistributedInit()) {
         allReduce(numBoxesArray);
       }
       numBoxes = numBoxesArray.scalar<int>();
       numBoxes = std::min(numBoxes / fl::getWorldSize() , 1);
+      std::cout << "Rank: " << fl::getWorldRank() << " Num boxes post" << numBoxes << std::endl;
 
 
       // TODO clamp number of boxes based on world size
