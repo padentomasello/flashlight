c64060f42c48d850cb85fef25522cea301e74006
Paden Tomasello <padentomasello@devfair049.maas>
2021-02-10 16:30:05 -0800

    --amend

--- flashlight/app/objdet/dataset/Coco.h
+++ flashlight/app/objdet/dataset/Coco.h
@@ -4,11 +4,8 @@
 #include "flashlight/ext/image/fl/dataset/Jpeg.h"
 #include "flashlight/ext/image/af/Transforms.h"
 #include "flashlight/fl/dataset/datasets.h"
+#include "flashlight/app/objdet/dataset/BatchTransformDataset.h"
 
-#include <iostream>
-
-DECLARE_bool(onesize);
-DECLARE_bool(equal_aspect);
 
 namespace fl {
 namespace app {
@@ -24,82 +21,6 @@ struct CocoData {
   std::vector<af::array> target_labels;
 };
 
-template<class T>
-using BatchTransformFunction = std::function<
-T(const std::vector<std::vector<af::array>>&)>;
-
-template<typename T>
-class BatchTransformDataset {
-
-public:
-  BatchTransformDataset(
-    std::shared_ptr<const Dataset> dataset,
-    int64_t batchsize,
-    BatchDatasetPolicy policy /* = BatchDatasetPolicy::INCLUDE_LAST */,
-    BatchTransformFunction<T> batchFn)
-   : dataset_(dataset),
-      batchSize_(batchsize),
-      batchPolicy_(policy),
-      batchFn_(batchFn) {
-    if (!dataset_) {
-      throw std::invalid_argument("dataset to be batched is null");
-    }
-    if (batchSize_ <= 0) {
-      throw std::invalid_argument("invalid batch size");
-    }
-    preBatchSize_ = dataset_->size();
-    switch (batchPolicy_) {
-      case BatchDatasetPolicy::INCLUDE_LAST:
-        size_ = ceil(static_cast<double>(preBatchSize_) / batchSize_);
-        break;
-      case BatchDatasetPolicy::SKIP_LAST:
-        size_ = floor(static_cast<double>(preBatchSize_) / batchSize_);
-        break;
-      case BatchDatasetPolicy::DIVISIBLE_ONLY:
-        if (size_ % batchSize_ != 0) {
-          throw std::invalid_argument(
-              "dataset is not evenly divisible into batches");
-        }
-        size_ = ceil(static_cast<double>(preBatchSize_) / batchSize_);
-        break;
-      default:
-        throw std::invalid_argument("unknown BatchDatasetPolicy");
-    }
-  }
-
-  ~BatchTransformDataset() {};
-
-  T get(const int64_t idx) {
-    if (!(idx >= 0 && idx < size())) {
-      throw std::out_of_range("Dataset idx out of range");
-    }
-    //dataset_->checkIndexBounds(idx);
-    std::vector<std::vector<af::array>> buffer;
-
-    int64_t start = batchSize_ * idx;
-    int64_t end = std::min(start + batchSize_, preBatchSize_);
-
-    for (int64_t batchidx = start; batchidx < end; ++batchidx) {
-      auto fds = dataset_->get(batchidx);
-      buffer.emplace_back(fds);
-    }
-    return batchFn_(buffer);
-  }
-
-  int64_t size() const {
-    return size_;
-  }
-
-private:
-  std::shared_ptr<const Dataset> dataset_;
-  int64_t batchSize_;
-  BatchDatasetPolicy batchPolicy_;
-  BatchTransformFunction<T> batchFn_;
-
-  int64_t preBatchSize_; // Size of the dataset before batching
-  int64_t size_;
-
-};
 
 class CocoDataset {
 
@@ -142,19 +63,6 @@ private:
   std::shared_ptr<ShuffleDataset> shuffled_;
 
 };
-
-//std::shared_ptr<CocoDataset> coco(
-    //const std::string& list_file,
-    //std::vector<ImageTransform>& transformfns,
-    //int batch_size);
-
-//af::array cxcywh_to_xyxy(const af::array& bboxes);
-
-//af::array xyxy_to_cxcywh(const af::array& bboxes);
-
-//af::array xywh_to_cxcywh(const af::array& bboxes);
-
-//af::array box_iou(const af::array& bboxes1, const af::array& bboxes2);
 
 } // namespace objdet
 } // namespace app
